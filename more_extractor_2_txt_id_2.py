38d3c4aa86d152483e9a6771bea9ad76f01a99df
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2022-11-08 22:34:21 -0500

    Update more_extractor_2_txt_id_2.py

--- more_extractor_2_txt_id_2.py
+++ more_extractor_2_txt_id_2.py
@@ -36,8 +36,7 @@ def button_one():
 
         ### OpenAI API code - obain a key and account here.###
         os.environ["OPENAI_API_KEY"] = st.secrets["openai_api_key"]
-        #os.environ["OPENAI_API_KEY"] = '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'
-
+        
         ### Begin GPT-3 text embedding search. Code and explanation for GPT-3 embeddgins found here: ###
         datafile_path = "./more_index_embeddings.csv"  # for your convenience, we precomputed the embeddings
         df = pd.read_csv(datafile_path)
