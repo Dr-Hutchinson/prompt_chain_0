8480fa6766db7437bf4381e8afb048e4821e9f8e
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-02 23:04:33 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -1,4 +1,4 @@
-#from langchain.prompts import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.prompts import FewShotPromptTemplate
 from langchain.chains import LLMChain
 from langchain import OpenAI, SerpAPIWrapper
