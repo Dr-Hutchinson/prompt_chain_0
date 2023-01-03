86c51117f669575f2ef32b6fd1ab2b4d218e368f
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-03 00:45:06 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -268,15 +268,20 @@ def button_one():
 
             llm = OpenAI(model_name="text-davinci-003", max_tokens = 1000, temperature=0.0)
             chain2 = LLMChain(llm=llm, prompt=prompt_from_string_examples6)
-            final_answer = chain2.run(submission_text + "\n" + str(section) + "\n3. Object of the Question\n" + knowledge_check + "\n6. Research Summary\n" + reasoning + "\n7. Research Summary Answer:\n" + output)
+            final_answer = chain2.run(submission_text + "\n" + section + "\n3. Object of the Question\n" + knowledge_check + "\n6. Research Summary\n" + reasoning + "\n7. Research Summary Answer:\n" + output)
             st.write('3. Analysis complete.')
+            st.subheader("Here's your question:")
+            st.write(submission_text)
+            st.subheader("Here is the text section you seleted:")
+            st.write(section)
             st.subheader("Here is GPT-3's Answer:")
             st.write(final_answer)
             st.subheader("Here is how GPT-3 Got it's Answer:")
-            st.write(section)
-            st.subheader("Section should be before here")
+            st.markdown("**Question Consideration:")
             st.write(knowledge_check)
+            st.markdown("**Search Summary:**")
             st.write(reasoning)
+            st.markdown("**GPT-3's Answer:")
             st.write(output)
 
         def ask_a_source():
