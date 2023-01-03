6fad4e39b9acdf7599aa773b016d35f275e60482
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-02 23:39:14 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -143,13 +143,10 @@ def button_one():
             df = pd.read_csv(datafile_path, encoding='latin1')
             section_select = r"Summary: Section_{}(:|$)".format(section_number)
             result = df[df['combined'].str.contains(section_select, regex=True)]
-
-            if not result.empty:
-              # Select the 'combined' column of the result DataFrame
-              result = result.loc[:, 'combined']
-
-                    # Do something with the resulting cell value
-              section = result.iloc[0]
+            # Select the 'combined' column of the result DataFrame
+            result = result.loc[:, 'combined']
+            # Do something with the resulting cell value
+            section = result.iloc[0]
 
 
             # self-search experiment 0.3 - revised question prompt examples
