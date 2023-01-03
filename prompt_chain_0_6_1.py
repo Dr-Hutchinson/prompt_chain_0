1500ab86a42cbca37a7f89dae4cc7cfe7c02faec
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-03 00:17:40 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -196,6 +196,7 @@ def button_one():
             llm = OpenAI(model_name="text-davinci-003", max_tokens = 500, temperature=0.0)
             chain1 = LLMChain(llm=llm, prompt=prompt_from_string_examples5)
             knowledge_check = chain1.run(submission_text+str(section))
+            st.write('1. Question Considered')
 
             # pass on revised quesiton for search
 
@@ -267,7 +268,11 @@ def button_one():
             llm = OpenAI(model_name="text-davinci-003", max_tokens = 1000, temperature=0.0)
             chain2 = LLMChain(llm=llm, prompt=prompt_from_string_examples6)
             final_answer = chain2.run(submission_text + "\n" + str(section) + "\n3. Object of the Question\n" + knowledge_check + "\n6. Research Summary\n" + reasoning + "\n7. Research Summary Answer:\n" + output)
+            st.write('3. Analysis complete.')
+            st.subheader("Here is GPT-3's Answer:")
             st.write(final_answer)
+
+            
 
         def ask_a_source():
 
