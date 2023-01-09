39fdb9d783875c5d8fafa92499e6334ba4caca2d
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-09 15:11:15 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -48,9 +48,9 @@ with col1:
     #<embed src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="800" height="800">
     #""", unsafe_allow_html=True)
     #pdf_display = F'<iframe src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="700" height="1000" type="application/pdf"></iframe>'
-    pdf_url = 'https://github.com/Dr-Hutchinson/prompt_chain_0/blob/main/annotated_full_text.pdf'
-    pdf_display = F'<iframe src="{pdf_url}" width="700" height="700" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    #pdf_url = 'https://github.com/Dr-Hutchinson/prompt_chain_0/blob/main/annotated_full_text.pdf'
+    #pdf_display = F'<iframe src="{pdf_url}" width="700" height="700" type="application/pdf"></iframe>'
+    #st.markdown(pdf_display, unsafe_allow_html=True)
 
     #file_path = "./annotated_full_text.pdf"
 
@@ -61,8 +61,9 @@ with col1:
         #st.markdown(pdf_display, unsafe_allow_html=True)
 
     #show_pdf("./annotated_full_text.pdf")
-
-
+    st.markdown("""
+    <embed src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="800" height="800">
+    """, unsafe_allow_html=True)
 
     st.markdown("**More's text has been broken up into different sections for enable GPT-3 to access it.**")
     datafile_path = "./more_index_combined.csv"
