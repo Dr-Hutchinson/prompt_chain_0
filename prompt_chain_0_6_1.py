3dfcd845d149235ec4c5e8055415e60b9962c33d
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-09 16:17:20 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -49,19 +49,19 @@ with col1:
     #""", unsafe_allow_html=True)
     #pdf_display = F'<iframe src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="700" height="1000" type="application/pdf"></iframe>'
     #pdf_url = 'https://github.com/Dr-Hutchinson/prompt_chain_0/blob/main/annotated_full_text.pdf'
-    pdf_url = 'https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf'
-    pdf_display = F'<iframe src="{pdf_url}" width="700" height="700" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    #pdf_url = 'https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf'
+    #pdf_display = F'<iframe src="{pdf_url}" width="700" height="700" type="application/pdf"></iframe>'
+    #st.markdown(pdf_display, unsafe_allow_html=True)
 
     #file_path = "./annotated_full_text.pdf"
 
-    #def show_pdf(file_path):
-        #with open(file_path,"rb") as f:
-            # base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        #pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-        #st.markdown(pdf_display, unsafe_allow_html=True)
+    def show_pdf(file_path):
+        with open(file_path,"rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+        st.markdown(pdf_display, unsafe_allow_html=True)
 
-    #show_pdf("./annotated_full_text.pdf")
+    show_pdf("./annotated_full_text.pdf")
 
     #file_path = './annotated_full_text.pdf'
     #def show_pdf(file_path):
