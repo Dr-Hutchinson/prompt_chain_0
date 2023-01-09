d024431d4417be285c8df28b050de09d29b83554
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-09 16:26:29 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -55,22 +55,22 @@ with col1:
 
     #file_path = "./annotated_full_text.pdf"
 
-    def show_pdf(file_path):
-        with open(file_path,"rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
-
-    show_pdf("./Richard_rm.pdf")
-
-    #file_path = './annotated_full_text.pdf'
     #def show_pdf(file_path):
         #with open(file_path,"rb") as f:
-                #base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        #pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+            #base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        #pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100" height="100" type="application/pdf">'
         #st.markdown(pdf_display, unsafe_allow_html=True)
 
-    #show_pdf('annotated_full_text.pdf')
+    #show_pdf("./Richard_rm.pdf")
+
+    #file_path = './annotated_full_text.pdf'
+    def show_pdf(file_path):
+        with open(file_path,"rb") as f:
+                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+    show_pdf('./Richard_rm.pdf')
 
 
     file_path = './annotated_full_text.pdf'
