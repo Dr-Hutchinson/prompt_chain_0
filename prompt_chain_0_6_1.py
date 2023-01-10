bcffce19fad644dbe75f6b99d61ee99facc95482
Dr-Hutchinson <45830997+Dr-Hutchinson@users.noreply.github.com>
2023-01-09 20:06:21 -0500

    Update prompt_chain_0_6_1.py

--- prompt_chain_0_6_1.py
+++ prompt_chain_0_6_1.py
@@ -45,8 +45,8 @@ with col1:
     #df = pd.read_csv('richardbot1_data.csv')
     #st.dataframe(df, height=500)
     st.markdown("""
-    #<embed src="https://raw.githubusercontent.com/Dr-Hutchinson/prompt_chain_0/c2f3795f25683fb194e11072abb9d586964896a2/annotated_full_text.pdf" width="800" height="800">
-    #""", unsafe_allow_html=True)
+    <embed src="https://raw.githubusercontent.com/Dr-Hutchinson/prompt_chain_0/c2f3795f25683fb194e11072abb9d586964896a2/annotated_full_text.pdf" width="800" height="800">
+    """, unsafe_allow_html=True)
     #pdf_display = F'<iframe src="https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf" width="700" height="1000" type="application/pdf"></iframe>'
     #pdf_url = 'https://github.com/Dr-Hutchinson/prompt_chain_0/blob/main/annotated_full_text.pdf'
     #pdf_url = 'https://thomasmorestudies.org/wp-content/uploads/2020/09/Richard.pdf'
